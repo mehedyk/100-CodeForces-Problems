b2600006d8e1cfa52cdc0973d8eee72915c18f95
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-06-04 23:06:23 +0600

    Create 020_Drinks.cpp

---
+++ 020_Drinks.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    double sum=0;
+    cin>>n;
+    int divident=n;
+    while(n--){
+        double m;
+        cin>>m;
+        sum+=m;
+    }
+    cout<<fixed<<setprecision(12)<<sum/divident;
+}
