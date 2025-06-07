8b208b318b1de5daea8c2ff9634102c03eea1737
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-06-08 00:11:01 +0600

    Update 023_AmusingJoke.cpp

--- 023_AmusingJoke.cpp
+++ 023_AmusingJoke.cpp
@@ -1,14 +1,13 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-    string guest, host, pile;
-    cin >> guest >> host >> pile;
+int main(){
+    string a,b,c;
+    cin>>a>>b>>c;
+    string d=a+b;
+    
+    sort(d.begin(),d.end());
+    sort(c.begin(),c.end());
 
-    string combined = guest + host;
-
-    sort(combined.begin(), combined.end());
-    sort(pile.begin(), pile.end());
-
-    if (combined == pile) cout << "YES";
-    else cout << "NO";
+    if(d==c) cout<<"YES";
+    else cout<<"NO";
 }
