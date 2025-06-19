a273d80dae4282706ba139710e7c29f627182651
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-06-20 00:45:04 +0600

    Update 024_SoftDrinking.cpp

--- 024_SoftDrinking.cpp
+++ 024_SoftDrinking.cpp
@@ -1,15 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n,k,l,c,d,p,nl,np;
-    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
-    int total_drinks=k*l;
-    int total_slices=c*d;
-
-    int toast_from_drink = total_drinks / nl;
-    int toast_from_limes = total_slices;
-    int toast_from_salt = p / np;
-
-    int total_toasts = min({toast_from_drink, toast_from_limes, toast_from_salt});
-    cout<<total_toasts/n;
+   int n,k,l,c,d,p,nl,np;
+   cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+   int total_drink=k*l,total_slices=c*d;
+   
+   int toast = min(min(total_drink / (n * nl), total_slices / n), p / (n*np));
+   cout<<toast;
 }
