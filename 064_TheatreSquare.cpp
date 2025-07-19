b5c5ff292eae7c9d9a2f89e6daa310c12cab3299
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-07-19 22:04:19 +0600

    Create 064_TheatreSquare.cpp

---
+++ 064_TheatreSquare.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n,m,a;
+    cin>>n>>m>>a;
+
+    long long row=(n+a-1)/a;
+    long long col=(m+a-1)/a;
+
+    cout <<row*col;
+}
